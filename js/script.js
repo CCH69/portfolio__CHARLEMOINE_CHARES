0cb8df523aba4367e2b4376ac65436b686a6eae6
CCH69 <charlemoine.c@gmail.com>
2022-10-10 11:18:11 +0200

    wip portfolio

--- js/script.js
+++ js/script.js
@@ -8,38 +8,40 @@ document.addEventListener("DOMContentLoaded", function () {
 var lastScrollTop = 0;
 // Cursor
 
-var cursor = document.querySelector(".cursor");
-var cursorinner = document.querySelector(".cursor2");
-var a = document.querySelectorAll("a");
+if (window.innerWidth > 480) {
+	var cursor = document.querySelector(".cursor");
+	var cursorinner = document.querySelector(".cursor2");
+	var a = document.querySelectorAll("a");
 
-document.addEventListener("mousemove", function (e) {
-	var x = e.clientX;
-	var y = e.clientY;
-	cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
-});
-
-document.addEventListener("mousemove", function (e) {
-	var x = e.clientX;
-	var y = e.clientY;
-	cursorinner.style.left = x + "px";
-	cursorinner.style.top = y + "px";
-});
-
-document.addEventListener("mousedown", function () {
-	cursor.classList.add("click");
-	cursorinner.classList.add("cursorinnerhover");
-});
-
-document.addEventListener("mouseup", function () {
-	cursor.classList.remove("click");
-	cursorinner.classList.remove("cursorinnerhover");
-});
-
-a.forEach((item) => {
-	item.addEventListener("mouseover", () => {
-		cursor.classList.add("hover");
+	document.addEventListener("mousemove", function (e) {
+		var x = e.clientX;
+		var y = e.clientY;
+		cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
 	});
-	item.addEventListener("mouseleave", () => {
-		cursor.classList.remove("hover");
+
+	document.addEventListener("mousemove", function (e) {
+		var x = e.clientX;
+		var y = e.clientY;
+		cursorinner.style.left = x + "px";
+		cursorinner.style.top = y + "px";
 	});
-});
+
+	document.addEventListener("mousedown", function () {
+		cursor.classList.add("click");
+		cursorinner.classList.add("cursorinnerhover");
+	});
+
+	document.addEventListener("mouseup", function () {
+		cursor.classList.remove("click");
+		cursorinner.classList.remove("cursorinnerhover");
+	});
+
+	a.forEach((item) => {
+		item.addEventListener("mouseover", () => {
+			cursor.classList.add("hover");
+		});
+		item.addEventListener("mouseleave", () => {
+			cursor.classList.remove("hover");
+		});
+	});
+}
